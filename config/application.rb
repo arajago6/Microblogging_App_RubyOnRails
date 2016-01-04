c88b6288110d8b81746ba60460984b8161905ae7
arajago6 <arajago6@hawk.iit.edu>
2016-01-04 00:55:07 -0600

    Finishing MBlo version 1.0

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,10 @@ module SampleApp
 
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    # Set asset path
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
+
     #Precompile additional assets
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
